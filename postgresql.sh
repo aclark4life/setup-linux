060ae7a72020fa43fcd5e79e2a17c722d120c28a
Alex Clark <aclark@aclark.net>
2022-05-06 19:36:44 -0400

    Update

--- postgresql.sh
+++ postgresql.sh
@@ -4,4 +4,6 @@ sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 apt update
 apt install -y postgresql-14
-
+su - postgres
+createuser -r -s alexclark
+exit
