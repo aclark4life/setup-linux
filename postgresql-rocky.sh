ae06bfffc4e3eabd7fbc5d27e7ebfe4883b3128f
Alex Clark <aclark@aclark.net>
2023-10-10 13:15:53 -0400

    Update

--- postgresql-rocky.sh
+++ postgresql-rocky.sh
@@ -1,9 +1,7 @@
-# Via https://www.postgresql.org/download/linux/debian/ 
-
-sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-apt update
-apt install -y postgresql-14
-su - postgres
-createuser -r -s alexclark
-exit
+# Via https://www.postgresql.org
+sudo dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-9-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+sudo dnf -qy module disable postgresql
+sudo dnf install -y postgresql16-server
+sudo /usr/pgsql-16/bin/postgresql-16-setup initdb
+sudo systemctl enable postgresql-16
+sudo systemctl start postgresql-16
